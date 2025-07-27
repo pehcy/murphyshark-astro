f800b20f9a2aadfb23138223379fee8f40797e6b
pehcy <cheeyung.peh@gmail.com>
2025-07-27 18:26:03 +0800

    Added Dark Them toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     markdown: {
         shikiConfig: {
-            theme: 'kanagawa-wave'
+            theme: 'min-dark'
         }
     }
 });
