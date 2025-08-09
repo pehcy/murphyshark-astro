a3fda86cb72024aee4e21c265069de5c571a959e
pehcy <cheeyung.peh@gmail.com>
2025-08-09 17:14:54 +0800

    Added sitemap to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        shikiConfig: {
-            theme: 'min-dark'
-        }
-    }
+  markdown: {
+      shikiConfig: {
+          theme: 'min-dark'
+      }
+  },
+
+  integrations: [sitemap()]
 });
